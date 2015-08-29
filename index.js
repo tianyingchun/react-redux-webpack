44d196751d888d5db95c426b188097bffc1cd51d
tianyingchun <tianyingchun@outlook.com>
2015-08-29 19:41:29 +0800

    initial project for react

---
+++ index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import App from './containers/App';
+import './css/bootstrap.css';
+import './css/index.css';
+
+React.render(
+  <App />,
+  document.getElementById('root')
+);
